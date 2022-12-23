516a7aff9c29dff308fbdfde29bf1a28d2684432
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2022-12-23 12:57:42 +0530

    addes some other pages

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,13 @@
+import { Skeleton } from "@mui/material";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import React from "react";
+import React, { useState } from "react";
 import { useAccount } from "wagmi";
 
 import "../styles/dashboard.scss";
 
 function Dashboard() {
   const { address, isConnected } = useAccount();
+  const [loading, setLoading] = useState(false);
 
   if (isConnected) {
     return (
@@ -17,13 +19,29 @@ function Dashboard() {
   return (
     <div className="db-main">
       <div className="db-sub">
-        <h1>Connect to Superfluid</h1>
+        <h1>
+          {loading ? (
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              sx={{ bgcolor: "grey.100" }}
+            />
+          ) : (
+            "Connect to Superfluid"
+          )}
+        </h1>
         <p>Connect your wallet, view any wallet, or take a look around!</p>
         <div className="db-grid-sub">
-          <div className="grid-sub">
-            <span className="grid-sub-title">Get Super Tokens</span>
-            <span className="grid-sub-info">Wrap any token in your wallet</span>
-          </div>
+          {loading ? (
+            <Skeleton animation="wave" variant="rounded" height={"100%"} />
+          ) : (
+            <div className="grid-sub">
+              <span className="grid-sub-title">Get Super Tokens</span>
+              <span className="grid-sub-info">
+                Wrap any token in your wallet
+              </span>
+            </div>
+          )}
           <div className="grid-sub">
             <span className="grid-sub-title">Get Super Tokens</span>
             <span className="grid-sub-info">Wrap any token in your wallet</span>
--- src/components/IDAIndex.js
+++ src/components/IDAIndex.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "../styles/dashboard.scss";
+import "../styles/ida.scss";
 
 function IDAIndex() {
-  return <div>IDA</div>;
+  return (
+    <div className="db-main">
+      <div className="db-sub">
+        <h1 className="ida-h1">Create IDA index</h1>
+        <p className="ida-p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+          rem?
+        </p>
+        <div className="ida-create-index-btn">
+          <button>Create Index</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default IDAIndex;
