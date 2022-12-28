01b84993072a75c820008e5516db66a25b791f48
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2022-12-28 12:47:16 +0530

    added functionality of edit and remove subscriber

--- src/components/Agreements.js
+++ src/components/Agreements.js
@@ -30,6 +30,7 @@ function Agreements() {
                   <tr>
                     <th>Subscribers</th>
                     <th>Units</th>
+                    <th></th>
                   </tr>
                 </thead>
                 <tbody>
@@ -44,6 +45,38 @@ function Agreements() {
                       </div>
                     </td>
                     <td>10</td>
+                    <td>
+                      <div className="edit-delete-buttons">
+                        <button className="edit-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                          </svg>
+                        </button>
+                        <button className="delete-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path
+                              d="M0 0h24v24H0V0z"
+                              fill="none"
+                              opacity=".87"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
                   </tr>
                   {/* ******** table data map ********** */}
                   <tr>
@@ -56,6 +89,38 @@ function Agreements() {
                       </div>
                     </td>
                     <td>5</td>
+                    <td>
+                      <div className="edit-delete-buttons">
+                        <button className="edit-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                          </svg>
+                        </button>
+                        <button className="delete-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path
+                              d="M0 0h24v24H0V0z"
+                              fill="none"
+                              opacity=".87"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
                   </tr>
                   {/* ******** table data map ********** */}
                   {/* ******** table data map ********** */}
@@ -69,6 +134,38 @@ function Agreements() {
                       </div>
                     </td>
                     <td>5</td>
+                    <td>
+                      <div className="edit-delete-buttons">
+                        <button className="edit-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                          </svg>
+                        </button>
+                        <button className="delete-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path
+                              d="M0 0h24v24H0V0z"
+                              fill="none"
+                              opacity=".87"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
                   </tr>
                   {/* ******** table data map ********** */}
                   {/* ******** table data map ********** */}
@@ -82,6 +179,38 @@ function Agreements() {
                       </div>
                     </td>
                     <td>5</td>
+                    <td>
+                      <div className="edit-delete-buttons">
+                        <button className="edit-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                          </svg>
+                        </button>
+                        <button className="delete-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path
+                              d="M0 0h24v24H0V0z"
+                              fill="none"
+                              opacity=".87"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
                   </tr>
                   {/* ******** table data map ********** */}
                   {/* ******** table data map ********** */}
@@ -95,6 +224,38 @@ function Agreements() {
                       </div>
                     </td>
                     <td>5</td>
+                    <td>
+                      <div className="edit-delete-buttons">
+                        <button className="edit-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                          </svg>
+                        </button>
+                        <button className="delete-subscriber-button">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                            fill="#000000"
+                          >
+                            <path
+                              d="M0 0h24v24H0V0z"
+                              fill="none"
+                              opacity=".87"
+                            />
+                            <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
                   </tr>
                   {/* ******** table data map ********** */}
                 </tbody>
--- src/components/SubscriberList.js
+++ src/components/SubscriberList.js
@@ -1,49 +1,174 @@
-import React from "react";
+import React, { useState } from "react";
+import { FormControl, MenuItem, Select } from "@mui/material";
+import Blokies from "./Blokies";
 
 function SubscriberList({ setInfo, setAdd, setList, setApprove }) {
+  const [indexValue, setIndexValue] = useState("");
+  const [amount, setAmount] = useState();
+
+  const handleChange = (e) => {
+    setIndexValue(e.target.value);
+  };
+
   return (
     <div className="db-sub">
       <h1 className="subscriber-h1">Subscribers</h1>
       <p className="subscriber-p">
         List of all the subscribers with Unit(proportion) and Index number.
       </p>
-      <div className="subscriber-add-box-table">
+      <div className="distribute-box">
+        <FormControl required fullWidth>
+          {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
+          <Select
+            displayEmpty
+            id="demo-simple-select"
+            value={indexValue}
+            onChange={handleChange}
+            sx={{
+              margin: "10px 0px",
+              color: "rgba(18, 20, 30, 0.87)",
+              fontSize: "1rem",
+              padding: "0px 5px",
+              ".css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.MuiSelect-select":
+                {
+                  minHeight: "auto",
+                },
+              ".MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(224, 224, 224)",
+                boxShadow: "rgba(204, 204, 204, 0.25) 0px 0px 6px 3px",
+                borderRadius: "15px",
+              },
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(224, 224, 224)",
+                boxShadow: "rgba(204, 204, 204, 0.25) 0px 0px 6px 3px",
+                borderRadius: "15px",
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgb(224, 224, 224)",
+                boxShadow: "rgba(204, 204, 204, 0.25) 0px 0px 6px 3px",
+                borderRadius: "15px",
+              },
+              ".MuiSvgIcon-root ": {
+                fill: "black",
+              },
+            }}
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem disabled value="">
+              <h4 className="index-placeholder">Select Index</h4>
+            </MenuItem>
+            <MenuItem value={45152}>#45152</MenuItem>
+            <MenuItem value={85698}>#85698</MenuItem>
+            <MenuItem value={95679}>#95679</MenuItem>
+          </Select>
+        </FormControl>
         {/* <h3>Subscriber Address</h3> */}
-        <div className="subscriber-list">
-          <table className="subscriber-list-table">
+        {/* <div className="subscriber-input-div">
+          <input
+            type="text"
+            className="subscriber-input-index"
+            placeholder="Subscriber Address"   
+          />
+        </div> */}
+        {/* <h3>Unit</h3> */}
+        <div className="distribute-subscribers-list">
+          <table>
             <thead>
               <tr>
-                <th>Subscriber</th>
-                <th>Unit</th>
-                <th>IDA index</th>
+                <th>Subscribers</th>
+                <th>Units</th>
+                <th>Edit</th>
               </tr>
             </thead>
             <tbody>
+              {/* ******** table data map ********** */}
               <tr>
                 <td>
-                  <span className="table-subscriber-address">
-                    0xeB88DDaEdA2261298F1b740137B2ae35aA42A975
-                  </span>
+                  <div className="blokies-and-address">
+                    <Blokies />
+                    <span className="subscriber-address">
+                      0xeB88DDaEdA2261298F1b740137B2ae35aA42A975
+                    </span>
+                  </div>
                 </td>
-                <td>Germany</td>
-                <td>#34567</td>
+                <td>10</td>
+                <td>
+                  <div className="edit-delete-buttons">
+                    <button className="edit-subscriber-button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        width="24px"
+                        fill="#000000"
+                      >
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                      </svg>
+                    </button>
+                    <button className="delete-subscriber-button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        width="24px"
+                        fill="#000000"
+                      >
+                        <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
+                        <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
               </tr>
+              {/* ******** table data map ********** */}
               <tr>
                 <td>
-                  <span className="table-subscriber-address">
-                    0xeB88DDaEdA2261298F1b740137B2ae35aA42A975
-                  </span>
+                  <div className="blokies-and-address">
+                    <Blokies />
+                    <span className="subscriber-address">
+                      0xeB88DDaEdA2261298F1b740137B2ae35aA42A975
+                    </span>
+                  </div>
                 </td>
-                <td>Mexico</td>
-                <td>#34567</td>
+                <td>5</td>
+                <td>
+                  <div className="edit-delete-buttons">
+                    <button className="edit-subscriber-button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        width="24px"
+                        fill="#000000"
+                      >
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                      </svg>
+                    </button>
+                    <button className="delete-subscriber-button">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                        width="24px"
+                        fill="#000000"
+                      >
+                        <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
+                        <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm4.3 14.3c-.39.39-1.02.39-1.41 0L12 13.41 9.11 16.3c-.39.39-1.02.39-1.41 0-.39-.39-.39-1.02 0-1.41L10.59 12 7.7 9.11c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0L12 10.59l2.89-2.89c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41L13.41 12l2.89 2.89c.38.38.38 1.02 0 1.41z" />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
               </tr>
+              {/* ******** table data map ********** */}
             </tbody>
-            <tfoot></tfoot>
           </table>
+          <div className="inside-subscriber-list"></div>
         </div>
-        <div className="subscriber-add-btn">
-          <button>Add Subscriber</button>
-        </div>
+        {/* <div className="distribute-btn">
+          <button>Distribute</button>
+        </div> */}
       </div>
     </div>
   );
