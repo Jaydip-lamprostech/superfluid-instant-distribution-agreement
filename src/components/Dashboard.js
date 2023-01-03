1b467d310612e1ac5f924bdc1a25ffcdf53543d7
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2023-01-03 12:24:15 +0530

    changes in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -216,7 +216,7 @@ function Dashboard() {
                 </table>
               </div>
               <button className="add-fund-btn" onClick={() => AddFunds()}>
-                Add 1000 Wei Contract
+                Add 1000 Wei in Contract
               </button>
             </div>
           </div>
